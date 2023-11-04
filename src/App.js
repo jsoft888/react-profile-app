99e0b2438f0e47744ec96fbb21a8d0f956bae083
MAC <mac@MACs-MacBook-Pro.local>
2023-11-03 21:57:13 -0700

    adding react-profile-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "John Oluwafemi",
+        bio: "A software developer",
+        imgSrc: "https://scontent-los2-1.xx.fbcdn.net/v/t1.6435-9/37420331_1943090552422628_402342524720513024_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=be3454&_nc_ohc=ZyjDUp60YYUAX9lgKLL&_nc_oc=AQlnt--sTP7iwPdxgc81JpaYEpXJiHMaOwIgk0h2CowCi2YDL2QI2hqiM4v7NZkncb0&_nc_ht=scontent-los2-1.xx&oh=00_AfA1yMX6x0H5Cs_XnhZgXoFuw7NQzJ4zmOUElFtluJXMKQ&oe=656D91D2",
+        profession: "Web Developer",
+      },
+      shows: false,
+      mountTime: new Date(),
+    };
+  }
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({ shows: !prevState.shows }));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(this.updateMountTime, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  updateMountTime = () => {
+    this.setState({ mountTime: new Date() });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleProfile}>
+          Toggle Profile
+        </button>
+        {this.state.shows && (
+          <div>
+            <h2>{this.state.person.fullName}</h2>
+            <p>{this.state.person.bio}</p>
+            <img src={this.state.person.imgSrc} alt="Avatar" />
+            <p>Profession: {this.state.person.profession}</p>
+          </div>
+        )}
+        <p>Time since mount: {Math.floor((new Date() - this.state.mountTime) / 1000)} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
